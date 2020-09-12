cf924e71ede9434129107cd13b5c3dcf0bd012cd
Joao vitor <resetgamer1@gmail.com>
2020-09-12 10:21:12 -0300

    config: orm migrations path

--- backend/dist/tsconfig-paths-bootstrap.js
+++ backend/dist/tsconfig-paths-bootstrap.js
@@ -1,7 +1,7 @@
 "use strict";
-var tsConfig = require("./tsconfig.json");
+var tsConfig = require("../tsconfig.json");
 var tsConfigPaths = require("tsconfig-paths");
-var baseUrl = "./"; // Either absolute or relative path. If relative it's resolved to current working directory.
+var baseUrl = "../"; // Either absolute or relative path. If relative it's resolved to current working directory.
 var cleanup = tsConfigPaths.register({
     baseUrl: baseUrl,
     paths: tsConfig.compilerOptions.paths,
