be4556b1bf64785779462e91f2d0ba1ccbd6c683
Joao vitor <resetgamer1@gmail.com>
2020-09-13 19:42:52 -0300

    Removed all files ignored

--- backend/dist/config/auth.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var environment_1 = require("../shared/utils/environment");
-exports.default = {
-    secret: environment_1.AUTH_SECRET,
-    expiresIn: environment_1.TOKEN_EXPIRATION_TIME,
-};
