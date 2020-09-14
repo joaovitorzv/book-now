d1f0730cc4aac146ba40fa8899fb1e37e8ee7169
Joao vitor <resetgamer1@gmail.com>
2020-09-14 17:16:34 -0300

    fix: added env vars to heroku

--- backend/src/modules/users/infra/http/controllers/SessionsController.ts
+++ backend/src/modules/users/infra/http/controllers/SessionsController.ts
@@ -8,18 +8,8 @@ class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
 
-    try {
-      container.resolve(AuthenticateUserService);
-    } catch (error) {
-      console.log(error)
-    }
     const authenticateUser = container.resolve(AuthenticateUserService);
 
-    try {
-      await authenticateUser.execute({ email, password });
-    } catch (error) {
-      console.log(error);
-    }
     const { user, token } = await authenticateUser.execute({ email, password });
 
     return response.status(200).json({ user: classToClass(user), token });
